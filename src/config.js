92df3874df54756fc92625493dba09ee7aacff7c
LeviMcPherson <growwithlevi@gmail.com>
2026-04-10 08:08:35 -0400

    Add src/config.js

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export const API_BASE = "https://bpqhfnhcxlilgcouiesx.supabase.co/functions/v1"
+export const TENANT_ID = "a0000000-0000-0000-0000-000000000001"
+export const USER_EMAIL = "growwithlevi@gmail.com"
+
+export const dashboardUrl = `${API_BASE}/n3xos-dashboard-data?tenant_id=${TENANT_ID}&user_email=${USER_EMAIL}`
+export const pageUrl = (page) => `${API_BASE}/n3xos-page-data?page=${page}&tenant_id=${TENANT_ID}&user_email=${USER_EMAIL}`
